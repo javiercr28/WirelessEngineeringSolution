19615a372628ddb3360d2d0cc6a798a08283c352
javiercr28 <nautilus28c@gmail.com>
2017-09-13 12:18:57 -0500

    Archivos para Termostato Wireless

--- Rasp/Bluetooth1.py
+++ Rasp/Bluetooth1.py
@@ -1,1 +1,26 @@
-import
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- coding: utf-850 -*-
+
+#Titulo				:Bluetooth1.py
+#Descripción		:Comunicación Bluetooth.
+#Autor          	:Javier Campos Rojas
+#Fecha            	:Setiembre-2017
+#Versión         	:1.0
+#Notas          	:
+#==============================================================================
+
+import os
+import glob
+import time
+import RPi.GPIO as GPIO
+from bluetooth import *
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-term')
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setuo(14, GPIO.OUT)
+
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(
