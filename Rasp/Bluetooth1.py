75d216a2a56f3ff51ceea610123f1cee93b2ae4e
javiercr28 <nautilus28c@gmail.com>
2017-09-15 11:41:43 -0500

    Actualización

--- Rasp/Bluetooth1.py
+++ Rasp/Bluetooth1.py
@@ -15,16 +15,39 @@ import glob
 import time
 import RPi.GPIO as GPIO
 from bluetooth import *
+from xbee import ZigBee
+from xbee.thread import XBee
+import serial
 
 #os.system('modprobe w1-gpio')
 #os.system('modprobe w1-term')
 
+GPIO.setwarnings(False);
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(14, GPIO.OUT)
 
 #base_dir = '/sys/bus/w1/devices/'
 #device_folder = glob.glob(base_dir + '28*')[0]
 #device_file = device_folder + '/w1_slave'
+
+
+PORT = '/dev/ttyUSB0'
+BAUD_RATE = 9600
+
+# Open serial port
+ser = serial.Serial(PORT, BAUD_RATE)
+
+# Create API object
+#xbee = XBee(ser)
+xbee = ZigBee(ser,escaped=True)
+
+def read_temp():
+    try:
+        response = xbee.wait_read_frame()
+        tempval=response['samples'][0]['adc-3']
+        return tempval
+    except KeyboardInterrupt:
+        ser.close()
 
 """
 def read_temp_raw():
@@ -36,7 +59,7 @@ def read_temp_raw():
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
+       e time.sleep(0.2)
         lines = read_temp_raw()
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
@@ -56,48 +79,48 @@ server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+#uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+uuid = "2f840c69-cecb-4b10-87e4-01b9d28c231c"
 
 advertise_service( server_sock, "AquaPiServer",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ],
-#                   protocols = [ OBEX_UUID ]
-                    )
+	service_id = uuid,
+	service_classes = [ uuid, SERIAL_PORT_CLASS ],
+	profiles = [ SERIAL_PORT_PROFILE ],
+	#protocols = [ OBEX_UUID ]
+	)
 while True:
 	print "Waiting for connection on RFCOMM channel %d" % port
-
 	client_sock, client_info = server_sock.accept()
 	print "Accepted connection from ", client_info
-
+	client_sock.send("Tempe")
 	try:
-	        data = client_sock.recv(1024)
-        	if len(data) == 0: break
-	        print "received [%s]" % data
-        """
-		if data == 'temp':
-			data = str(read_temp())+'!'
-		el
-        """
-        if data == 'lightOn':
-			GPIO.output(13,False)
-			data = 'light on!'
-		elif data == 'lightOff':
-			GPIO.output(13,True)
-			data = 'light off!'
-		else:
-			data = 'WTF!'
-	        client_sock.send(data)
-		print "sending [%s]" % data
+		while True:
+			data = client_sock.recv(1024)
+			if len(data) == 0: break
+			print "received [%s]" % data
+			if data == 'temp':
+				#data = 'Temperatura = '
+				data = 'Temperatura = '+ str(read_temp())
+			elif data == '111':
+				GPIO.output(14,GPIO.HIGH)
+				data = 'light on!'
+			elif data == '000':
+				GPIO.output(14,GPIO.LOW)
+				data = 'light off!'
+			elif data == 'out':
+				GPIO.output(14,GPIO.LOW)
+				client_sock.close()
+				break
+			else:
+				data = 'WTF!'
+			client_sock.send(data)
+			print "sending [%s]" % data
 	except IOError:
-		pass
-
+		break
+		
 	except KeyboardInterrupt:
-
 		print "disconnected"
-
 		client_sock.close()
 		server_sock.close()
 		print "all done"
-
-        break
+		break
